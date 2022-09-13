370edf0d65cdab86651f0f26d728c1215f923a23
Xuân Huy <itnxhuy1806@gmail.com>
2022-09-13 15:25:41 +0700

    [BackEnd] Update Middleware

--- src/utils/logger.middleware.ts
+++ src/utils/logger.middleware.ts
@@ -1,12 +1,18 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { UsersService } from 'src/database/user/index.service';
 
 @Injectable()
-export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    // console.log('header: ', req.headers.cookie);
-    console.log('body: ', req.body);
-    console.log('params: ', req.params);
+export class Middleware implements NestMiddleware {
+  constructor(private readonly usersService: UsersService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
     next();
+    // let user = undefined;
+    // if (user) user = await this.usersService.findOne(req.headers.authorization);
+    // console.log('🚀 ~ file: logger.middleware.ts ~ line 11 ~ Middleware ~ use ~ user', user);
+    // console.log('body: ', req.body);
+    // if (user) next();
+    // else return res.status(401).json('Unauthorized');
   }
 }

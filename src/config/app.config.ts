98687fdf318feefd191d18309077a7ed9fe0e3e9
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-10-21 14:39:28 +0700

    Remove Cors block, because I want to migrate FE server to vercel

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,10 +1,8 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => {
-  let cors: any = process.env.CORS;
-  if (process.env.NODE_ENV !== 'product') cors = '*';
   return {
     port: parseInt(process.env.APP_PORT, 10) || 3333,
-    cors,
+    cors: '*',
   };
 });
